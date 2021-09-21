d16b432b948b96c8f418f6c0d216a12cd53179a3
Mateusz Stadnik <matgla@live.com>
2021-09-21 21:54:47 +0200

    generate arrays/buffers

--- source/GL/gl.cpp
+++ source/GL/gl.cpp
@@ -15,66 +15,58 @@
 
 #include "GL/gl.h"
 
-#include "io.hpp"
+#include <algorithm>
 
+#include "io.hpp"
 #include "messages/ack.hpp"
-#include "messages/clear_screen.hpp"
 #include "messages/begin_primitives.hpp"
+#include "messages/clear_screen.hpp"
 #include "messages/end_primitives.hpp"
+#include "messages/generate_names.hpp"
 #include "messages/write_vertex.hpp"
 
-#include "io.hpp"
-
-void glInit(GLenum mode) 
+void glInit(GLenum mode)
 {
-
 }
 
 uint8_t convert_mode(GLenum mode)
 {
     switch (mode)
     {
-        case GL_TRIANGLES: return PrimitiveType::triangle;
-        case GL_QUADS: return PrimitiveType::square; 
+    case GL_TRIANGLES:
+        return PrimitiveType::triangle;
+    case GL_QUADS:
+        return PrimitiveType::square;
     }
     return PrimitiveType::none;
 }
 
 void glBegin(GLenum mode)
 {
-    BeginPrimitives msg { .type = convert_mode(mode) };
+    BeginPrimitives msg{.type = convert_mode(mode)};
     write_msg(wr_id, msg);
 }
 
-void glEnd() 
+void glEnd()
 {
-    EndPrimitives msg; 
+    EndPrimitives msg;
     write_msg(wr_id, msg);
 }
 
-void glNormal3fv(const GLfloat* v)
+void glNormal3fv(const GLfloat *v)
 {
-
 }
 
-void glVertex3fv(const GLfloat* v)
+void glVertex3fv(const GLfloat *v)
 {
-    WriteVertex msg {
-        .x = v[0],
-        .y = v[1],
-        .z = v[2]
-    };
+    WriteVertex msg{.x = v[0], .y = v[1], .z = v[2]};
 
     write_msg(wr_id, msg);
 }
 
 void glVertex3f(GLfloat x, GLfloat y, GLfloat z)
 {
-    WriteVertex msg {
-        .x = x,
-        .y = y, 
-        .z = z
-    };
+    WriteVertex msg{.x = x, .y = y, .z = z};
 
     write_msg(wr_id, msg);
 }
@@ -88,7 +80,7 @@ void glClear(GLbitfield mask)
 void glEnable(GLenum cap)
 {
 }
-void glLightfv(GLenum light, GLenum pname, const GLfloat* params)
+void glLightfv(GLenum light, GLenum pname, const GLfloat *params)
 {
 }
 void glMatrixMode(GLenum mode)
@@ -104,27 +96,89 @@ void glRotatef(GLfloat angle, GLfloat x, GLfloat y, GLfloat z)
 
 constexpr uint8_t to_rgb332(float r, float g, float b)
 {
-    return static_cast<uint8_t>(r * 7) << 5
-        | static_cast<uint8_t>(g * 7) << 2
-        | static_cast<uint8_t>(b * 3);
+    return static_cast<uint8_t>(r * 7) << 5 | static_cast<uint8_t>(g * 7) << 2 |
+           static_cast<uint8_t>(b * 3);
 }
 
 void glClearColor(GLfloat red, GLfloat green, GLfloat blue, GLfloat alpha)
 {
-    
 }
-
 
 void glEnableClientState(GLenum attrib)
 {
 }
 
+void __generateNames(GLsizei n, GLuint *arrays, ObjectType type)
+{
+    uint32_t loops = n / sizeof(GenerateNamesResponse::data);
+
+    uint8_t elements =
+        std::min(static_cast<std::size_t>(n), n % sizeof(GenerateNamesResponse::data));
+    std::size_t index = 0;
+    if (elements)
+    {
+        GenerateNamesRequest req{
+            .elements = elements,
+            .type     = type,
+        };
+        write_msg(wr_id, req);
+
+        GenerateNamesResponse resp;
+        printf("Wait for ids\n");
+        read_msg(rd_id, resp);
+
+        if (resp.error_code != 0)
+        {
+            printf("Received error code for GenerateNames: %d\n", resp.error_code);
+            std::abort();
+        }
+
+        for (; index < elements; ++index)
+        {
+            arrays[index] = resp.data[index];
+        }
+    }
+    for (uint32_t i = 0; i < loops; ++i)
+    {
+        uint8_t elements = 30;
+        GenerateNamesRequest req{
+            .elements = sizeof(GenerateNamesResponse::data),
+            .type     = type,
+        };
+        write_msg(wr_id, req);
+
+        GenerateNamesResponse resp;
+        read_msg(rd_id, resp);
+
+        if (resp.error_code != 0)
+        {
+            printf("Received error code for GenerateNames: %d\n", resp.error_code);
+            std::abort();
+        }
+
+        for (int i = 0; i < sizeof(GenerateNamesResponse::data); ++i)
+        {
+            arrays[index] = resp.data[i];
+            ++index;
+        }
+    }
+
+    printf("Received ids: {");
+    for (int i = 0; i < n; ++i)
+    {
+        printf("%d,", arrays[i]);
+    }
+    printf("\n");
+}
+
 void glGenVertexArrays(GLsizei n, GLuint *arrays)
 {
+    __generateNames(n, arrays, ObjectType::VertexArray);
 }
 
 void glGenBuffers(GLsizei n, GLuint *buffers)
 {
+    __generateNames(n, buffers, ObjectType::Buffer);
 }
 
 void glBindBuffer(GLenum target, GLuint buffer)
@@ -139,7 +193,8 @@ void glBufferData(GLenum target, GLsizeiptr size, const void *data, GLenum usage
 {
 }
 
-void glVertexAttribPointer(GLuint index, GLint size, GLenum type, GLboolean normalized, GLsizei stride, const void *pointer)
+void glVertexAttribPointer(GLuint index, GLint size, GLenum type, GLboolean normalized,
+                           GLsizei stride, const void *pointer)
 {
 }
 
@@ -159,8 +214,7 @@ GLuint glCreateProgram(void)
 {
 }
 
-void glAttachShader(GLuint program, 
-    GLuint shader)
+void glAttachShader(GLuint program, GLuint shader)
 {
 }
 
